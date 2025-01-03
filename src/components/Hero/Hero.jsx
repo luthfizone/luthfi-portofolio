03dd3e5fc1e7398cb443fa8138511ff132d41dac
luthfizone <luthfizonedev@gmail.com>
2025-01-03 11:44:15 +0700

    initialize react first portofolio

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Luthfi</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 3 years of experience using React,
+          NodeJS, and Wordpress. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:luthfizonedev@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/hero.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
